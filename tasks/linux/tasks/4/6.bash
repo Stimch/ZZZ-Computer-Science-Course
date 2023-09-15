8a9e5c1ec6999d0e1b0c5db21c385fcc8f577d58
root <root@DESKTOP-3KDM0H0>
2023-09-15 22:56:17 +0300

    zad6

--- tasks/linux/tasks/4/6.bash
+++ tasks/linux/tasks/4/6.bash
@@ -1,1 +1,2 @@
-# Найдите все py файлы, содержащие в названии любое из трёх слов: backdoor, trojan, keylogger.
+ Найдите все py файлы, содержащие в названии любое из трёх слов: backdoor, trojan, keylogger.
+ find -type f -name "backdoor.py" -o -name "trojan.py" -o -name "keylogger.py"
