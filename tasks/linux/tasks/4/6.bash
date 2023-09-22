d9149106c64993514c40df0e9e3b68623cb2f94b
root <root@DESKTOP-3KDM0H0>
2023-09-22 16:35:53 +0300

    zadanie6

--- tasks/linux/tasks/4/6.bash
+++ tasks/linux/tasks/4/6.bash
@@ -1,2 +1,2 @@
  Найдите все py файлы, содержащие в названии любое из трёх слов: backdoor, trojan, keylogger.
- find -type f -name "backdoor.py" -o -name "trojan.py" -o -name "keylogger.py"
+ find -type f -iname "*backdoor*.py" -o -iname "*trojan*.py" -o -iname "*keylogger*.py"
