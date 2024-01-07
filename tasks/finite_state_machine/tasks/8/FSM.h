2cd0c0f1ec88dd6a3862e2ea6cfddf49a124a62e
root <root@DESKTOP-3KDM0H0>
2024-01-07 20:04:16 +0300

    try6

--- tasks/finite_state_machine/tasks/8/FSM.h
+++ tasks/finite_state_machine/tasks/8/FSM.h
@@ -2,7 +2,7 @@
 
 #include "fsm_realization.h"
 
-void Task() {
+int Task(void) {
     const char* in_file_name = "../tasks/finite_state_machine/tasks/8/in.txt";
     const char* out_file_name = "../tasks/finite_state_machine/tasks/8/out.txt";
     StateMachine* sm = MakeStateMachine(in_file_name, out_file_name);
@@ -14,4 +14,5 @@ void Task() {
         }
     }
     DeleteStateMachine(sm);
+    return 0;
 }
