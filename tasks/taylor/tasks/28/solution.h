d30122a5106873f69103b11f6e80028918be02b5
root <root@DESKTOP-3KDM0H0>
2024-01-07 15:17:00 +0300

    taylor28

--- tasks/taylor/tasks/28/solution.h
+++ tasks/taylor/tasks/28/solution.h
@@ -7,11 +7,11 @@
 
 const long double A = M_PI / 5;
 const long double B = (6 * M_PI) / 5;
-const long double epsilon = LDBL_EPSILON;
+const long double EPSILON = LDBL_EPSILON;
 const int32_t K = 100000;
 const int32_t N = 10;
 const int32_t MAX_ITERATIONS = 100;
-const long double step = (B - A) / N;
+const long double STEP = (B - A) / N;
 
 long double Function(long double x) {
     return (logl(fabs(2.0 * sin(x / 2.0))) * (-1.0));
@@ -23,7 +23,7 @@ long double Taylor(int32_t n, long double x) {
 
 int Task() {
     long double x = A;
-    long double sum;
+    long double sum = 0;
     int32_t iteration = 1;
     printf("\n==========================================================================\n");
     printf("|| Step\t x\t            Sum of row            function            i     ||\n");
@@ -31,7 +31,7 @@ int Task() {
     while (x <= B) {
         while (iteration < MAX_ITERATIONS) {
             sum += Taylor(iteration, x);
-            if (fabsl(sum - Function(x)) >= epsilon * K) {
+            if (fabsl(sum - Function(x)) >= EPSILON * K) {
                 iteration += 1;
             } else {
                 printf(" %0.2Lf | %20.20Lf | %20.15Lf | %5d |\n", x, sum, Function(x), iteration);
@@ -41,7 +41,7 @@ int Task() {
                 break;
             }
         }
-        x += step;
+        x += STEP;
     }
     return 0;
 }
