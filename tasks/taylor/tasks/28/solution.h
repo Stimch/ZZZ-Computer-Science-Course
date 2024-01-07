821206bd3f3d4b6207faac7325c210cc532b71ed
root <root@DESKTOP-3KDM0H0>
2024-01-07 15:14:33 +0300

    taylor28

--- tasks/taylor/tasks/28/solution.h
+++ tasks/taylor/tasks/28/solution.h
@@ -1,15 +1,47 @@
 #pragma once
 
 #include <float.h>
+#include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 
-long double Func(long double x) {
-    // write your function here
-    return 0.0;
+const long double A = M_PI / 5;
+const long double B = (6 * M_PI) / 5;
+const long double epsilon = LDBL_EPSILON;
+const int32_t K = 100000;
+const int32_t N = 10;
+const int32_t MAX_ITERATIONS = 100;
+const long double step = (B - A) / N;
+
+long double Function(long double x) {
+    return (logl(fabs(2.0 * sin(x / 2.0))) * (-1.0));
+}
+
+long double Taylor(int32_t n, long double x) {
+    return (cos(n * x) / n);
 }
 
 int Task() {
-    // write your solution here
+    long double x = A;
+    long double sum;
+    int32_t iteration = 1;
+    printf("\n==========================================================================\n");
+    printf("|| Step\t x\t            Sum of row            function            i     ||\n");
+    printf("\n======================================================================\n");
+    while (x <= B) {
+        while (iteration < MAX_ITERATIONS) {
+            sum += Taylor(iteration, x);
+            if (fabsl(sum - Function(x)) >= epsilon * K) {
+                iteration += 1;
+            } else {
+                printf(" %0.2Lf | %20.20Lf | %20.15Lf | %5d |\n", x, sum, Function(x), iteration);
+                printf("-------------------------------------------------------------\n");
+                iteration = 1;
+                sum = 0;
+                break;
+            }
+        }
+        x += step;
+    }
     return 0;
 }
