a063c9cbdb8ebc8223dc43d0db8f6c7294e522ee
root <root@DESKTOP-3KDM0H0>
2024-01-07 22:48:18 +0300

    lin18_final

--- tasks/linerization/tasks/18/solution.h
+++ tasks/linerization/tasks/18/solution.h
@@ -17,15 +17,6 @@ Matrix CreateMatrix(int lines) {
     return matrix;
 }
 
-Matrix FillMatrix(Matrix matrix, int32_t lines, int32_t columns) {
-    for (int32_t i = 0; i < lines; ++i) {
-        for (int32_t j = 0; j < columns; ++j) {
-            scanf("%d", &matrix[i][j]);
-        }
-    }
-    return matrix;
-}
-
 void PrintMatrix(Matrix matrix, int32_t lines, int32_t columns) {
     for (int32_t i = 0; i < lines; ++i) {
         for (int32_t j = 0; j < columns; ++j) {
@@ -33,6 +24,25 @@ void PrintMatrix(Matrix matrix, int32_t lines, int32_t columns) {
         }
         printf("\n");
     }
+}
+
+void InitializeMatrix(Matrix matrix) {
+    matrix[0][0] = 1;
+    matrix[0][1] = 2;
+    matrix[0][2] = 3;
+    matrix[0][3] = 4;
+    matrix[1][0] = 5;
+    matrix[1][1] = 6;
+    matrix[1][2] = 7;
+    matrix[1][3] = 8;
+    matrix[2][0] = 9;
+    matrix[2][1] = 10;
+    matrix[2][2] = 11;
+    matrix[2][3] = 12;
+    matrix[3][0] = 13;
+    matrix[3][1] = 14;
+    matrix[3][2] = 15;
+    matrix[3][3] = 16;
 }
 
 void GoThroughMatrix(Matrix matrix, int32_t* a, int32_t swing, int32_t lines, int32_t columns, int32_t steps) {
@@ -85,7 +95,7 @@ int Task() {
     int32_t columns = M;
     int32_t size = lines * columns;
     Matrix matrix = CreateMatrix(lines);
-    matrix = FillMatrix(matrix, lines, columns);
+    InitializeMatrix(matrix);
     printf("The Original Matrix:\n");
     PrintMatrix(matrix, lines, columns);
     int32_t a[N * M];  // массив для хранения результата
